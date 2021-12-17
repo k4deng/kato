71c9b237ffbc5667a19d78ce43eb2008fe9fd5ef
k4deng <70725766+k4deng@users.noreply.github.com>
2021-12-17 01:40:22 +0000

    changed logs to write to a file aswell for later viewing and problem solving and changed some names

--- modules/Logger.js
+++ modules/Logger.js
@@ -3,29 +3,41 @@ Logger class for easy and aesthetically pleasing console logging
 */
 const chalk = require("chalk");
 const moment = require("moment");
+const	log = require('simple-node-logger').createRollingFileLogger({
+  logDirectory: './data/logs',
+  fileNamePattern: 'roll-<DATE>.log',
+  dateFormat: 'YYYY.MM.DD',
+});
 
 exports.log = (content, type = "log") => {
   const timestamp = `[${moment().format("YYYY-MM-DD HH:mm:ss")}]:`;
   switch (type) {
-    case "log": {
-      return console.log(`${timestamp} ${chalk.bgBlue(type.toUpperCase())} ${content} `);
-    }
-    case "warn": {
-      return console.log(`${timestamp} ${chalk.black.bgYellow(type.toUpperCase())} ${content} `);
-    }
-    case "error": {
-      return console.log(`${timestamp} ${chalk.bgRed(type.toUpperCase())} ${content} `);
-    }
-    case "debug": {
-      return console.log(`${timestamp} ${chalk.green(type.toUpperCase())} ${content} `);
-    }
-    case "cmd": {
-      return console.log(`${timestamp} ${chalk.black.bgWhite(type.toUpperCase())} ${content}`);
-    }
-    case "ready": {
-      return console.log(`${timestamp} ${chalk.black.bgGreen(type.toUpperCase())} ${content}`);
-    }
-    default: throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
+    case 'log':
+      log.info(content);
+      console.log(`${timestamp} ${chalk.bgBlue(type.toUpperCase())} ${content} `);
+      break;
+    case 'warn':
+      log.warn(content);
+      console.log(`${timestamp} ${chalk.black.bgYellow(type.toUpperCase())} ${content} `);
+      break;
+    case 'error':
+      log.error(content);
+      console.log(`${timestamp} ${chalk.bgRed(type.toUpperCase())} ${content} `);
+      break;
+    case 'debug':
+      log.debug(content);
+      console.log(`${timestamp} ${chalk.green(type.toUpperCase())} ${content} `);
+      break;
+    case 'cmd':
+      log.info(content);
+      console.log(`${timestamp} ${chalk.black.bgWhite(type.toUpperCase())} ${content}`);
+      break;
+    case 'ready':
+      log.info(content);
+      console.log(`${timestamp} ${chalk.black.bgGreen(type.toUpperCase())} ${content}`);
+      break;
+    default:
+      throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
   }
 }; 
 
